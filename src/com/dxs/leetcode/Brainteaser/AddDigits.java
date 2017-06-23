ef144f23b7b17164f925057e5faca79dc3275c2e
dengxusheng <dengxusheng@unionpay.com>
2017-06-23 11:38:03 +0800

    Add Digits

---
+++ src/com/dxs/leetcode/Brainteaser/AddDigits.java
@@ -0,0 +1,16 @@
+package com.dxs.leetcode.Brainteaser;
+/**
+ * @author dengxusheng	
+ * @version 创建时间：2017年6月23日 上午11:20:15
+ * 
+ */
+public class AddDigits {
+	public int addDigits(int num) {
+        return (num - 1) % 9 + 1;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
