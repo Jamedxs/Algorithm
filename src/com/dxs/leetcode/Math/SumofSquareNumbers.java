e87656e9ca01bc3808b849aa6f6ae2679087237c
dengxusheng <dengxusheng@unionpay.com>
2017-09-26 10:15:41 +0800

    Sum of Square Numbers

---
+++ src/com/dxs/leetcode/Math/SumofSquareNumbers.java
@@ -0,0 +1,38 @@
+package com.dxs.leetcode.Math;
+
+/**
+ * Created by dengxusheng on 2017-09-26.
+ * Given a non-negative integer c, your task is to decide whether there're two integers a and b such that a2 + b2 = c.
+ Example 1:
+
+ Input: 5
+ Output: True
+ Explanation: 1 * 1 + 2 * 2 = 5
+
+
+ Example 2:
+
+ Input: 3
+ Output: False
+
+ */
+public class SumofSquareNumbers {
+    public static boolean judgeSquareSum(int c) {
+        int a = 0;
+        int b = (int)Math.sqrt(c);
+        while(a <= b){
+            if(a*a + b*b == c){
+                return true;
+            }else if(a*a + b*b < c){
+                a++;
+            }else{
+                b--;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(judgeSquareSum(5));
+    }
+}
